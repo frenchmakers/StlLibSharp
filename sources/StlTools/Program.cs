6590712a7746408b1f3dcbba54bc725e50f9d2b0
Yan Grenier <ygrenier@ygrenier.com>
2016-01-26 23:21:11 +0100

    Create and prepare the console application stl-tools.

---
+++ sources/StlTools/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace StlTools
+{
+    class Program
+    {
+
+        [Conditional("DEBUG")]
+        static void PressKeyToQuit()
+        {
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Press a key to quit...");
+                Console.ReadKey();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+            PressKeyToQuit();
+        }
+    }
+}
